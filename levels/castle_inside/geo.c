ad50852a6f87ef2617e439038ecb181ad2d2a217
alchav <alchav@jalchavware.com>
2026-08-17 04:17:25 -0400

    Wall mounted signs hidden

--- levels/castle_inside/geo.c
+++ levels/castle_inside/geo.c
@@ -8,10 +8,34 @@
 #include "game/moving_texture.h"
 #include "game/screen_transition.h"
 #include "game/paintings.h"
+#include "sm64ap.h"
 
 #include "make_const_nonconst.h"
 
 #include "levels/castle_inside/header.h"
+
+static const Gfx *const sCastleWallSignDisplayLists[] = {
+    inside_castle_seg7_dl_07031830,
+    inside_castle_seg7_dl_07033158,
+    inside_castle_seg7_dl_07035288,
+    inside_castle_seg7_dl_07038350,
+    inside_castle_seg7_dl_0703A808,
+    inside_castle_seg7_dl_07043CD8,
+    inside_castle_seg7_dl_07066FA0,
+};
+
+Gfx *geo_sm64ap_castle_wall_sign(s32 callContext, struct GraphNode *node, UNUSED Mat4 *mtx) {
+    struct GraphNodeGenerated *generatedNode = (struct GraphNodeGenerated *) node;
+
+    if (callContext != GEO_CONTEXT_RENDER || !SM64AP_HaveSigns(LEVEL_CASTLE)
+        || generatedNode->parameter >= ARRAY_COUNT(sCastleWallSignDisplayLists)) {
+        return NULL;
+    }
+
+    generatedNode->fnNode.node.flags =
+        (generatedNode->fnNode.node.flags & 0xFF) | (LAYER_ALPHA << 8);
+    return (Gfx *) sCastleWallSignDisplayLists[generatedNode->parameter];
+}
 
 #include "levels/castle_inside/star_door/geo.inc.c"
 #include "levels/castle_inside/trap_door/geo.inc.c"
